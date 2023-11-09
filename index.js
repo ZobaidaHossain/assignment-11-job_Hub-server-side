d98de64817801a261fb50ea33bd1d0e2778f1379
ZobaidaHossain <zobaida15-13671@diu.edu.bd>
2023-11-09 18:58:24 +0600

    add job information get from db

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function run() {
     const coffeeCollection=client.db('jobHub').collection('addjob');
     const cartCollection=client.db('jobHub').collection('cart');
     const userCollection=client.db('jobHub').collection('user');
-    // const bookingCollectionff=client.db('jobHub').collection('myjobs');
+    // const bookingCollectionff=cliffent.db('jobHub').collection('myjobs');
 
 // add job old
    //send add a job to the database
